b7c9345ab2ecaed826364a7115069e60809a07b0
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-11-27 16:16:14 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# iris
-Deploy IRIS with CI/CD on Elestio
+# IRIS CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/iris"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy IRIS server with CI/CD on Elestio
+
+<img src="iris.gif" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open IRIS here:
+
+    URL: https://[CI_CD_DOMAIN]
+    login: "administrator"
+    password: "[ADMIN_PASSWORD]"
+
+You can open pgAdmin here:
+
+    URL: https://[CI_CD_DOMAIN]:29027
+    login: "[ADMIN_EMAIL]"
+    password: "[ADMIN_PASSWORD]"
---
+++ scripts/preInstall.sh
@@ -0,0 +1,30 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+IRIS_SECRET_KEY=$(openssl rand -hex 64)
+IRIS_SECURITY_PASSWORD_SALT=$(openssl rand -hex 64)
+IRIS_ADM_API_KEY=$(openssl rand -hex 64)
+
+cat << EOT >> ./.mautic_env
+
+IRIS_SECRET_KEY="${IRIS_SECRET_KEY}"
+IRIS_SECURITY_PASSWORD_SALT="${IRIS_SECURITY_PASSWORD_SALT}"
+IRIS_ADM_API_KEY="${IRIS_ADM_API_KEY}"
+EOT
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 29094,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
